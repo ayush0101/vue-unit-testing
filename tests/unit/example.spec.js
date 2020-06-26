a83a136ddaf9278447a492908ae159b6ff120063
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-26 08:01:27 +0530

    done

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,15 @@
 import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
+const wrapper = (component, propsData) => {
+  return shallowMount(component, {
+    propsData,
+  });
+};
+
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper(HelloWorld, { msg }).text()).toMatch(msg);
   });
 });
