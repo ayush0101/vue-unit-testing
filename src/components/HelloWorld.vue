4863462560bce95ad0a9f7e2983082c36966c17e
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-26 08:08:03 +0530

    done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -82,6 +82,11 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      counter: 0
+    };
   }
 };
 </script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -12,4 +12,18 @@ describe('HelloWorld.vue', () => {
     const msg = 'new message';
     expect(wrapper(HelloWorld, { msg }).text()).toMatch(msg);
   });
+
+  it('has local data as a function', () => {
+    expect(typeof HelloWorld.data).toBe('function');
+  });
+
+  it('has counter initialized to zero', () => {
+    const defaultData = HelloWorld.data();
+    expect(defaultData.counter).toBe(0);
+  });
+
+  //  at mounting
+  it('correctly sets the counter when created', () => {
+    expect(wrapper(HelloWorld).vm.$data.counter).toBe(0);
+  });
 });
